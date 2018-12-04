5a3ce3d925387b0cea17e2f72212e4721e3d834d
Janne Kauttio <janne.kauttio@pandia.fi>
2018-12-05 01:24:21 +0200

    Implement rest query for top x games from given market

--- src/main/java/com/gamerefinery/demo/TopGamesController.java
+++ src/main/java/com/gamerefinery/demo/TopGamesController.java
@@ -1,18 +1,20 @@
 package com.gamerefinery.demo;
 
 import com.mongodb.MongoClient;
+import com.mongodb.client.FindIterable;
 import com.mongodb.client.MongoCollection;
+import org.bson.Document;
 
 import javax.enterprise.context.RequestScoped;
 import javax.inject.Inject;
-import javax.ws.rs.GET;
-import javax.ws.rs.Path;
-import javax.ws.rs.Produces;
+import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
 import java.util.ArrayList;
 import java.util.List;
 
-import static com.mongodb.client.model.Filters.lt;
+import static com.mongodb.client.model.Filters.*;
+import static com.mongodb.client.model.Projections.*;
+import static com.mongodb.client.model.Sorts.*;
 
 @Path("/topgames")
 @RequestScoped
@@ -22,19 +24,26 @@ public class TopGamesController {
     private MongoClient mongoClient;
 
     @GET
-    @Path("/test")
+    @Path("/{market:(fi)|(it)|(es)}/{top:[0-9]+}")
     @Produces(MediaType.APPLICATION_JSON)
-    public List<Game> test() {
-        List<Game> results = new ArrayList<>();
-        for (Game game : getGameCollection().find(lt("rank", 50))) {
-            results.add(game);
+    public List<Document> getMarketTop(
+            @PathParam("market") String market,
+            @PathParam("top") int top,
+            @QueryParam("fields") List<String> fields
+    ) {
+        FindIterable<Document> iterable = getDocuments()
+                .find(and(eq("market", market), lte("rank", top)))
+                .sort(orderBy(ascending("rank")));
+
+        if (fields != null && !fields.isEmpty()) {
+            iterable = iterable.projection(include(fields));
         }
 
-        return results;
+        return iterable.into(new ArrayList<>());
     }
 
-    private MongoCollection<Game> getGameCollection() {
-        return mongoClient.getDatabase("tg").getCollection("games", Game.class);
+    private MongoCollection<Document> getDocuments() {
+        return mongoClient.getDatabase("tg").getCollection("games");
     }
 
 }
