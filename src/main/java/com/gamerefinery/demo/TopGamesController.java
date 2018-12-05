0fdb0bf5ac47b6425060c39439a1a0fd361ae9f2
Janne Kauttio <janne.kauttio@pandia.fi>
2018-12-05 02:22:27 +0200

    Add support for market entered query

--- src/main/java/com/gamerefinery/demo/TopGamesController.java
+++ src/main/java/com/gamerefinery/demo/TopGamesController.java
@@ -9,6 +9,8 @@ import javax.enterprise.context.RequestScoped;
 import javax.inject.Inject;
 import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
+import java.time.LocalDate;
+import java.time.ZoneId;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -40,6 +42,42 @@ public class TopGamesController {
         }
 
         return iterable.into(new ArrayList<>());
+    }
+
+    @GET
+    @Path("/{market:(fi)|(it)|(es)}/{top:(10|20|50)}/{days:[0-9]+}")
+    @Produces(MediaType.APPLICATION_JSON)
+    public List<Document> getMarketEntered(
+            @PathParam("market") String market,
+            @PathParam("top") int top,
+            @PathParam("days") int days,
+            @QueryParam("fields") List<String> fields
+    ) {
+        String timeKey = "top" + top + "Entry";
+        // apparently the time stamps in the database are epoch milliseconds?
+        long minTime = LocalDate.now().minusDays(days)
+                .atStartOfDay(ZoneId.of("UTC")).toInstant().toEpochMilli();
+        FindIterable<Document> iterable = getDocuments()
+                .find(and(eq("market", market), gte(timeKey, minTime)))
+                .sort(orderBy(descending(timeKey)));
+
+        if (fields != null && !fields.isEmpty()) {
+            iterable = iterable.projection(include(fields));
+        }
+
+        return iterable.into(new ArrayList<>());
+    }
+
+    @GET
+    @Path("/history/{id}/{market:(fi)|(it)|(es)}/{interval:d|w|m}")
+    @Produces(MediaType.APPLICATION_JSON)
+    public List<Document> getTitleHistory(
+            @PathParam("id") String id,
+            @PathParam("market") String market,
+            @PathParam("interval") String interval
+    ) {
+        // TODO: implementation
+        return new ArrayList<>();
     }
 
     private MongoCollection<Document> getDocuments() {
