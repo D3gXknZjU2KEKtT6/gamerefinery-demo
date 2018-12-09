56b584db100485d283ddecb4920c2515f83d3bc5
Janne Kauttio <janne.kauttio@pandia.fi>
2018-12-09 21:27:40 +0200

    Extend and refactor the REST interface
    The REST interface is extended with support for the "favorite" and "rank
    history" operations, which allow the user to specify certain games as
    favorites and obtain the rank history of a game. The old methods were
    also refactored to use appId instead of gameId as the identifier, since
    gameId seems to be undefined for several titles. The parameter orders
    were also modified to make them more consistent between the different
    methods.

--- src/main/java/com/gamerefinery/demo/TopGamesController.java
+++ src/main/java/com/gamerefinery/demo/TopGamesController.java
@@ -1,22 +1,27 @@
 package com.gamerefinery.demo;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.mongodb.MongoClient;
 import com.mongodb.client.FindIterable;
 import com.mongodb.client.MongoCollection;
+import com.mongodb.client.result.UpdateResult;
 import org.bson.Document;
+import org.bson.conversions.Bson;
 
 import javax.enterprise.context.RequestScoped;
 import javax.inject.Inject;
 import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
-import java.time.LocalDate;
-import java.time.ZoneId;
-import java.util.ArrayList;
-import java.util.List;
+import javax.ws.rs.core.Response;
+import java.time.*;
+import java.time.temporal.WeekFields;
+import java.util.*;
+import java.util.stream.Collectors;
 
 import static com.mongodb.client.model.Filters.*;
 import static com.mongodb.client.model.Projections.*;
 import static com.mongodb.client.model.Sorts.*;
+import static com.mongodb.client.model.Updates.*;
 
 @Path("/topgames")
 @RequestScoped
@@ -26,11 +31,74 @@ public class TopGamesController {
     private MongoClient mongoClient;
 
     @GET
-    @Path("/{market:(fi)|(it)|(es)}/{top:[0-9]+}")
+    @Path("/{id}")
+    @Produces(MediaType.APPLICATION_JSON)
+    public List<Document> findGame(
+            @PathParam("id") Long id, // appId seems like a reasonable id to use?
+            @QueryParam("fields") List<String> fields
+    ) {
+        FindIterable<Document> iterable = getDocuments()
+                .find(eq("appId", id));
+
+        if (fields != null && !fields.isEmpty()) {
+            iterable = iterable.projection(include(fields));
+        }
+
+        return iterable.into(new ArrayList<>());
+    }
+
+    @GET
+    @Path("/{id}/{market:(fi)|(it)|(es)}")
+    @Produces(MediaType.APPLICATION_JSON)
+    public List<Document> findGameInMarket(
+            @PathParam("id") Long id,
+            @PathParam("market") String market,
+            @QueryParam("fields") List<String> fields
+    ) {
+        FindIterable<Document> iterable = getDocuments()
+                .find(and(eq("appId", id), eq("market", market)));
+
+        if (fields != null && !fields.isEmpty()) {
+            iterable = iterable.projection(include(fields));
+        }
+
+        return iterable.into(new ArrayList<>());
+    }
+
+    @POST
+    @Path("/{id}/favorite")
+    @Consumes(MediaType.APPLICATION_JSON)
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response setFavorite(
+            @PathParam("id") Long id,
+            Boolean favorite
+    ) {
+        if (favorite == null) {
+            return Response.status(Response.Status.BAD_REQUEST).build();
+        }
+
+        Bson operation = favorite ? set("favorite", true) : unset("favorite");
+
+        UpdateResult result = getDocuments().updateMany(
+                eq("appId", id),
+                operation
+        );
+
+        if (!result.wasAcknowledged()) {
+            return Response.status(Response.Status.INTERNAL_SERVER_ERROR).build();
+        } else if (result.getMatchedCount() < 1) {
+            return Response.status(Response.Status.NOT_FOUND).build();
+        } else {
+            return Response.ok().build();
+        }
+    }
+
+    @GET
+    @Path("/top/{top:[0-9]+}/{market:(fi)|(it)|(es)}")
     @Produces(MediaType.APPLICATION_JSON)
     public List<Document> getMarketTop(
-            @PathParam("market") String market,
             @PathParam("top") int top,
+            @PathParam("market") String market,
             @QueryParam("fields") List<String> fields
     ) {
         FindIterable<Document> iterable = getDocuments()
@@ -45,11 +113,11 @@ public class TopGamesController {
     }
 
     @GET
-    @Path("/{market:(fi)|(it)|(es)}/{top:(10|20|50)}/{days:[0-9]+}")
+    @Path("/top/{top:(10|20|50)}/{market:(fi)|(it)|(es)}/{days:[0-9]+}")
     @Produces(MediaType.APPLICATION_JSON)
     public List<Document> getMarketEntered(
-            @PathParam("market") String market,
             @PathParam("top") int top,
+            @PathParam("market") String market,
             @PathParam("days") int days,
             @QueryParam("fields") List<String> fields
     ) {
@@ -69,19 +137,100 @@ public class TopGamesController {
     }
 
     @GET
-    @Path("/history/{id}/{market:(fi)|(it)|(es)}/{interval:d|w|m}")
+    @Path("/history/{id}/{market:(fi)|(it)|(es)}/{interval:(day)|(week)|(month)}")
     @Produces(MediaType.APPLICATION_JSON)
-    public List<Document> getTitleHistory(
-            @PathParam("id") String id,
+    public List<HistoryData> getTitleHistory(
+            @PathParam("id") Long id,
             @PathParam("market") String market,
             @PathParam("interval") String interval
     ) {
-        // TODO: implementation
-        return new ArrayList<>();
+        List<Document> history = getDocuments()
+                .find(and(eq("appId", id), eq("market", market)))
+                .first()
+                .get("history", ArrayList.class);
+
+        // group the history entries by timestamps to time intervals
+        Map<LocalDate, List<Integer>> ranks = history.stream()
+                .collect(Collectors.groupingBy(
+                        doc -> toInterval(interval, doc.getLong("ts")),
+                        Collectors.mapping(
+                                doc -> doc.getInteger("rank"),
+                                Collectors.toList()
+                        )
+                ));
+
+        // combine the grouped ranks within the time intervals to history data entries with some rudimentary
+        // statistics about the game rank within the time interval
+        return ranks.entrySet().stream()
+                .map(entry -> new HistoryData(entry.getKey(), entry.getValue()))
+                .sorted(Comparator.comparing(hd -> hd.getLocalDate()))
+                .collect(Collectors.toList());
     }
 
     private MongoCollection<Document> getDocuments() {
         return mongoClient.getDatabase("tg").getCollection("games");
+    }
+
+    private static class HistoryData {
+        LocalDate date;
+        Double avg;
+        Integer min;
+        Integer max;
+
+        HistoryData(LocalDate date, List<Integer> ranks) {
+            this.date = date;
+            this.avg = ranks.stream().mapToInt(Integer::intValue).average().getAsDouble();
+            this.min = ranks.stream().mapToInt(Integer::intValue).min().getAsInt();
+            this.max = ranks.stream().mapToInt(Integer::intValue).max().getAsInt();
+        }
+
+        // only return the serialized date string to the client
+        @JsonIgnore
+        public LocalDate getLocalDate() {
+            return date;
+        }
+
+        public String getTime() {
+            return date.toString();
+        }
+
+        public Double getAvg() {
+            return avg;
+        }
+
+        public Integer getMin() {
+            return min;
+        }
+
+        public Integer getMax() {
+            return max;
+        }
+    }
+
+    private static LocalDate toInterval(String param, long ts) {
+        switch (param) {
+            case "day": return toDay(toDateTime(ts));
+            case "week": return toWeek(toDateTime(ts));
+            case "month": return toMonth(toDateTime(ts));
+            default: throw new UnsupportedOperationException("unsupported time interval: " + param);
+        }
+    }
+
+    private static LocalDateTime toDateTime(long ts) {
+        // converts the time from utc to system time zone, not sure if this is wanted or not
+        return Instant.ofEpochMilli(ts).atZone(ZoneId.systemDefault()).toLocalDateTime();
+    }
+
+    private static LocalDate toDay(LocalDateTime dateTime) {
+        return dateTime.toLocalDate();
+    }
+
+    private static LocalDate toWeek(LocalDateTime dateTime) {
+        return dateTime.with(WeekFields.of(Locale.getDefault()).getFirstDayOfWeek()).toLocalDate();
+    }
+
+    private static LocalDate toMonth(LocalDateTime dateTime) {
+        return dateTime.withDayOfMonth(1).toLocalDate();
     }
 
 }
